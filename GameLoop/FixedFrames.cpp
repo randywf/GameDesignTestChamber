24f5786ba22e72f627372560af4cfbe7f4ea877d
randywf <5883513+randywf@users.noreply.github.com>
2020-03-14 13:08:39 -0600

    reorganized code, fixed edge case, removed class
    organized code into two main sections within loop.
    fixed case of initial iteration calculating an invalid tick rate.
    removed unnecessary gameclock class and used clock functions instead.

--- GameLoop/FixedFrames.cpp
+++ GameLoop/FixedFrames.cpp
@@ -1,62 +1,61 @@
-#include "GameClock.h"
-#include <iostream>
+#include <ctime>
 #include <iomanip>
+#include <iostream>
 
-// Define speed of game
+// Define tick rate of main loop
 const int TICKRATE = 5;
 const int SKIP_TICKS = 1000000000 / TICKRATE;
 
-// Loop section
 int main(void)
 {
-	GameClock gametime;
-	timespec current, previous;
-	timespec sleep_curr, sleep_prev;
-	int elapsed_ns, elapsed_s, ticks_per_sec, accumulated;
+	timespec current, previous; // time at start and end of main loop
+	timespec sleep_i, sleep_f; // time at start and end of sleep loop
+	int elapsed_ns, elapsed_s, ticks_per_sec, accumulator;
+	short first_loop = 1;
 
-	std::cout << "Beginning game loop..." << std::endl;
-	std::cout << "Start time: ";
-	std::cout << gametime.GetStartTime().tv_sec << "." << gametime.GetStartTime().tv_nsec << " seconds" << std::endl;
+	clock_gettime(CLOCK_MONOTONIC, &previous);
+	clock_gettime(CLOCK_MONOTONIC, &sleep_f);
+	accumulator = 0;
 
-	previous = gametime.GetCurrentTime();
-	accumulated = 0;
+	while(1) {
+		clock_gettime(CLOCK_MONOTONIC, &current);
 
-	while(1)
-	{
-		current = gametime.GetCurrentTime();
-
-		// Calculate elapsed time
-		// This is used to double check that the tickrate is correct
-		if (current.tv_sec == previous.tv_sec) {
-			elapsed_ns = current.tv_nsec - previous.tv_nsec;
-		}
-		else {
-			elapsed_s = current.tv_sec - previous.tv_sec;
-			elapsed_ns = current.tv_nsec + (elapsed_s * 1000000000) - previous.tv_nsec;
-		}
-
-		// Find TPS
-		ticks_per_sec = 1000000000 / elapsed_ns;
-
-		// sleep until one time step has passed
-		while (accumulated < SKIP_TICKS)
-		{
-			sleep_curr = gametime.GetCurrentTime();
-			if (sleep_curr.tv_nsec > sleep_prev.tv_nsec) {
-				accumulated += sleep_curr.tv_nsec - sleep_prev.tv_nsec;
+		if (first_loop != 1) {
+			// Calculate elapsed time
+			// This is used to double check that the tickrate is correct
+			// Don't run on first iteration of loop (there's no previous time)
+			if (current.tv_sec == previous.tv_sec) {
+				elapsed_ns = current.tv_nsec - previous.tv_nsec;
 			}
 			else {
-				accumulated += sleep_curr.tv_nsec + 1000000000 - sleep_prev.tv_nsec;
+				elapsed_s = current.tv_sec - previous.tv_sec;
+				elapsed_ns = current.tv_nsec + (elapsed_s * 1000000000) - previous.tv_nsec;
 			}
-			sleep_prev = sleep_curr;
+			// Find ticks per second
+			ticks_per_sec = 1000000000 / elapsed_ns;
+			std::cout << "Elapsed nanosecs:" << elapsed_ns << std::endl;
+			std::cout << "Ticks per second:" << ticks_per_sec << std::endl << std::endl;
 		}
-		while (accumulated >= SKIP_TICKS)
-		{
-			accumulated -= SKIP_TICKS;
+		else {
+			first_loop = 0;
 		}
 
-		std::cout << "Ticks per second:" << ticks_per_sec << std::endl;
-		
+		while (accumulator < SKIP_TICKS) {
+			// sleep until one time step has passed
+			clock_gettime(CLOCK_MONOTONIC, &sleep_i);
+			if (sleep_i.tv_sec == sleep_f.tv_sec) {
+				accumulator += sleep_i.tv_nsec - sleep_f.tv_nsec;
+			}
+			else {
+				accumulator += sleep_i.tv_nsec + (sleep_i.tv_sec - sleep_f.tv_sec) * 1000000000 - sleep_f.tv_nsec;
+			}
+			sleep_f = sleep_i;
+		}
+		while (accumulator >= SKIP_TICKS) {
+			accumulator -= SKIP_TICKS;
+		}
+
 		previous = current;
 	}
+	return 0;
 }
